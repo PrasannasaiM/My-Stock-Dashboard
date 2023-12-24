eaaa0f332dec09d7d3ec9fb08f26fc9195b42874
PrasannasaiM <153351542+PrasannasaiM@users.noreply.github.com>
2023-12-24 12:47:52 +0530

    Commit Home page

--- App.js
+++ App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Home from './component/Home';
+import About from './component/About';
+import Contact from './component/Contact';
+import Login from './component/Login';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<Home/>} />
+          <Route path="/Login" element={<Login/>} />
+          <Route path="/About" element={<About/>} />
+          <Route path="/Contact" element={<Contact/>} />
+        </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ component/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Home = () => {
+  return <h2>Stock Market Dashboard Home Page</h2>;
+};
+
+export default Home;
+
